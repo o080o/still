f8e6e0c6279a974f91a4c492290a9fe11410f658
o080o <o0o.moro.o0o@gmail.com>
2014-11-09 02:08:39 -0600

    fixed json module loading

--- still.lua
+++ still.lua
@@ -11,6 +11,7 @@ function Parser:parse(content, config)
 	local version = content:byte(5)
 	assert(version==tonumber('51',16), "The version is incompatible, Please use lua51 ")
 	self:parseData(content)
+	local json = loadJson()
 	return json:encode(self.tree), self.tree, self.config --json lib appended to this lua file
 end
 function Parser:parseData(content)
@@ -261,6 +262,7 @@ function Parser:parseInstruction(ins)
 	}}
 end
 
+
 -- -*- coding: utf-8 -*-
 --
 -- Simple JSON encoding and decoding in pure Lua.
@@ -298,34 +300,34 @@ local OBJDEF = {
 
 function OBJDEF:onEncodeError(message, etc)
    if etc ~= nil then
-      message = message .. " (" .. OBJDEF:encode(etc) .. ")"
+	  message = message .. " (" .. OBJDEF:encode(etc) .. ")"
    end
 
    if self.assert then
-      self.assert(false, message)
+	  self.assert(false, message)
    else
-      assert(false, message)
+	  assert(false, message)
    end
 end
 
 
 local function backslash_replacement_function(c)
    if c == "\n" then
-      return "\\n"
+	  return "\\n"
    elseif c == "\r" then
-      return "\\r"
+	  return "\\r"
    elseif c == "\t" then
-      return "\\t"
+	  return "\\t"
    elseif c == "\b" then
-      return "\\b"
+	  return "\\b"
    elseif c == "\f" then
-      return "\\f"
+	  return "\\f"
    elseif c == '"' then
-      return '\\"'
+	  return '\\"'
    elseif c == '\\' then
-      return '\\\\'
+	  return '\\\\'
    else
-      return string.format("\\u%04x", c:byte())
+	  return string.format("\\u%04x", c:byte())
    end
 end
 
@@ -356,71 +358,71 @@ local function object_or_array(self, T, etc)
    local maximum_number_key
 
    for key in pairs(T) do
-      if type(key) == 'string' then
-         table.insert(string_keys, key)
-      elseif type(key) == 'number' then
-         table.insert(number_keys, key)
-         if key <= 0 or key >= math.huge then
-            number_keys_must_be_strings = true
-         elseif not maximum_number_key or key > maximum_number_key then
-            maximum_number_key = key
-         end
-      else
-         self:onEncodeError("can't encode table with a key of type " .. type(key), etc)
-      end
+	  if type(key) == 'string' then
+		 table.insert(string_keys, key)
+	  elseif type(key) == 'number' then
+		 table.insert(number_keys, key)
+		 if key <= 0 or key >= math.huge then
+			number_keys_must_be_strings = true
+		 elseif not maximum_number_key or key > maximum_number_key then
+			maximum_number_key = key
+		 end
+	  else
+		 self:onEncodeError("can't encode table with a key of type " .. type(key), etc)
+	  end
    end
 
    if #string_keys == 0 and not number_keys_must_be_strings then
-      --
-      -- An empty table, or a numeric-only array
-      --
-      if #number_keys > 0 then
-         return nil, maximum_number_key -- an array
-      elseif tostring(T) == "JSON array" then
-         return nil
-      elseif tostring(T) == "JSON object" then
-         return { }
-      else
-         -- have to guess, so we'll pick array, since empty arrays are likely more common than empty objects
-         return nil
-      end
+	  --
+	  -- An empty table, or a numeric-only array
+	  --
+	  if #number_keys > 0 then
+		 return nil, maximum_number_key -- an array
+	  elseif tostring(T) == "JSON array" then
+		 return nil
+	  elseif tostring(T) == "JSON object" then
+		 return { }
+	  else
+		 -- have to guess, so we'll pick array, since empty arrays are likely more common than empty objects
+		 return nil
+	  end
    end
 
    table.sort(string_keys)
 
    local map
    if #number_keys > 0 then
-      --
-      -- If we're here then we have either mixed string/number keys, or numbers inappropriate for a JSON array
-      -- It's not ideal, but we'll turn the numbers into strings so that we can at least create a JSON object.
-      --
+	  --
+	  -- If we're here then we have either mixed string/number keys, or numbers inappropriate for a JSON array
+	  -- It's not ideal, but we'll turn the numbers into strings so that we can at least create a JSON object.
+	  --
 
-      if self.noKeyConversion then
-         self:onEncodeError("a table with both numeric and string keys could be an object or array; aborting", etc)
-      end
+	  if self.noKeyConversion then
+		 self:onEncodeError("a table with both numeric and string keys could be an object or array; aborting", etc)
+	  end
 
-      --
-      -- Have to make a shallow copy of the source table so we can remap the numeric keys to be strings
-      --
-      map = { }
-      for key, val in pairs(T) do
-         map[key] = val
-      end
+	  --
+	  -- Have to make a shallow copy of the source table so we can remap the numeric keys to be strings
+	  --
+	  map = { }
+	  for key, val in pairs(T) do
+		 map[key] = val
+	  end
 
-      table.sort(number_keys)
+	  table.sort(number_keys)
 
-      --
-      -- Throw numeric keys in there as strings
-      --
-      for _, number_key in ipairs(number_keys) do
-         local string_key = tostring(number_key)
-         if map[string_key] == nil then
-            table.insert(string_keys , string_key)
-            map[string_key] = T[number_key]
-         else
-            self:onEncodeError("conflict converting table with mixed-type keys into a JSON object: key " .. number_key .. " exists both as a string and a number.", etc)
-         end
-      end
+	  --
+	  -- Throw numeric keys in there as strings
+	  --
+	  for _, number_key in ipairs(number_keys) do
+		 local string_key = tostring(number_key)
+		 if map[string_key] == nil then
+			table.insert(string_keys , string_key)
+			map[string_key] = T[number_key]
+		 else
+			self:onEncodeError("conflict converting table with mixed-type keys into a JSON object: key " .. number_key .. " exists both as a string and a number.", etc)
+		 end
+	  end
    end
 
    return string_keys, nil, map
@@ -433,133 +435,133 @@ local encode_value -- must predeclare because it calls itself
 function encode_value(self, value, parents, etc, indent) -- non-nil indent means pretty-printing
 
    if value == nil then
-      return 'null'
+	  return 'null'
 
    elseif type(value) == 'string' then
-      return json_string_literal(value)
+	  return json_string_literal(value)
 
    elseif type(value) == 'number' then
-      if value ~= value then
-         --
-         -- NaN (Not a Number).
-         -- JSON has no NaN, so we have to fudge the best we can. This should really be a package option.
-         --
-         return "null"
-      elseif value >= math.huge then
-         --
-         -- Positive infinity. JSON has no INF, so we have to fudge the best we can. This should
-         -- really be a package option. Note: at least with some implementations, positive infinity
-         -- is both ">= math.huge" and "<= -math.huge", which makes no sense but that's how it is.
-         -- Negative infinity is properly "<= -math.huge". So, we must be sure to check the ">="
-         -- case first.
-         --
-         return "1e+9999"
-      elseif value <= -math.huge then
-         --
-         -- Negative infinity.
-         -- JSON has no INF, so we have to fudge the best we can. This should really be a package option.
-         --
-         return "-1e+9999"
-      else
-         return tostring(value)
-      end
+	  if value ~= value then
+		 --
+		 -- NaN (Not a Number).
+		 -- JSON has no NaN, so we have to fudge the best we can. This should really be a package option.
+		 --
+		 return "null"
+	  elseif value >= math.huge then
+		 --
+		 -- Positive infinity. JSON has no INF, so we have to fudge the best we can. This should
+		 -- really be a package option. Note: at least with some implementations, positive infinity
+		 -- is both ">= math.huge" and "<= -math.huge", which makes no sense but that's how it is.
+		 -- Negative infinity is properly "<= -math.huge". So, we must be sure to check the ">="
+		 -- case first.
+		 --
+		 return "1e+9999"
+	  elseif value <= -math.huge then
+		 --
+		 -- Negative infinity.
+		 -- JSON has no INF, so we have to fudge the best we can. This should really be a package option.
+		 --
+		 return "-1e+9999"
+	  else
+		 return tostring(value)
+	  end
 
    elseif type(value) == 'boolean' then
-      return tostring(value)
+	  return tostring(value)
 
    elseif type(value) ~= 'table' then
-      self:onEncodeError("can't convert " .. type(value) .. " to JSON", etc)
+	  self:onEncodeError("can't convert " .. type(value) .. " to JSON", etc)
 
    else
-      --
-      -- A table to be converted to either a JSON object or array.
-      --
-      local T = value
+	  --
+	  -- A table to be converted to either a JSON object or array.
+	  --
+	  local T = value
 
-      if parents[T] then
-         self:onEncodeError("table " .. tostring(T) .. " is a child of itself", etc)
-      else
-         parents[T] = true
-      end
+	  if parents[T] then
+		 self:onEncodeError("table " .. tostring(T) .. " is a child of itself", etc)
+	  else
+		 parents[T] = true
+	  end
 
-      local result_value
+	  local result_value
 
-      local object_keys, maximum_number_key, map = object_or_array(self, T, etc)
-      if maximum_number_key then
-         --
-         -- An array...
-         --
-         local ITEMS = { }
-         for i = 1, maximum_number_key do
-            table.insert(ITEMS, encode_value(self, T[i], parents, etc, indent))
-         end
+	  local object_keys, maximum_number_key, map = object_or_array(self, T, etc)
+	  if maximum_number_key then
+		 --
+		 -- An array...
+		 --
+		 local ITEMS = { }
+		 for i = 1, maximum_number_key do
+			table.insert(ITEMS, encode_value(self, T[i], parents, etc, indent))
+		 end
 
-         if indent then
-            result_value = "[ " .. table.concat(ITEMS, ", ") .. " ]"
-         else
-            result_value = "[" .. table.concat(ITEMS, ",") .. "]"
-         end
+		 if indent then
+			result_value = "[ " .. table.concat(ITEMS, ", ") .. " ]"
+		 else
+			result_value = "[" .. table.concat(ITEMS, ",") .. "]"
+		 end
 
-      elseif object_keys then
-         --
-         -- An object
-         --
-         local TT = map or T
+	  elseif object_keys then
+		 --
+		 -- An object
+		 --
+		 local TT = map or T
 
-         if indent then
+		 if indent then
 
-            local KEYS = { }
-            local max_key_length = 0
-            for _, key in ipairs(object_keys) do
-               local encoded = encode_value(self, tostring(key), parents, etc, "")
-               max_key_length = math.max(max_key_length, #encoded)
-               table.insert(KEYS, encoded)
-            end
-            local key_indent = indent .. "    "
-            local subtable_indent = indent .. string.rep(" ", max_key_length + 2 + 4)
-            local FORMAT = "%s%" .. string.format("%d", max_key_length) .. "s: %s"
+			local KEYS = { }
+			local max_key_length = 0
+			for _, key in ipairs(object_keys) do
+			   local encoded = encode_value(self, tostring(key), parents, etc, "")
+			   max_key_length = math.max(max_key_length, #encoded)
+			   table.insert(KEYS, encoded)
+			end
+			local key_indent = indent .. "    "
+			local subtable_indent = indent .. string.rep(" ", max_key_length + 2 + 4)
+			local FORMAT = "%s%" .. string.format("%d", max_key_length) .. "s: %s"
 
-            local COMBINED_PARTS = { }
-            for i, key in ipairs(object_keys) do
-               local encoded_val = encode_value(self, TT[key], parents, etc, subtable_indent)
-               table.insert(COMBINED_PARTS, string.format(FORMAT, key_indent, KEYS[i], encoded_val))
-            end
-            result_value = "{\n" .. table.concat(COMBINED_PARTS, ",\n") .. "\n" .. indent .. "}"
+			local COMBINED_PARTS = { }
+			for i, key in ipairs(object_keys) do
+			   local encoded_val = encode_value(self, TT[key], parents, etc, subtable_indent)
+			   table.insert(COMBINED_PARTS, string.format(FORMAT, key_indent, KEYS[i], encoded_val))
+			end
+			result_value = "{\n" .. table.concat(COMBINED_PARTS, ",\n") .. "\n" .. indent .. "}"
 
-         else
+		 else
 
-            local PARTS = { }
-            for _, key in ipairs(object_keys) do
-               local encoded_val = encode_value(self, TT[key],       parents, etc, indent)
-               local encoded_key = encode_value(self, tostring(key), parents, etc, indent)
-               table.insert(PARTS, string.format("%s:%s", encoded_key, encoded_val))
-            end
-            result_value = "{" .. table.concat(PARTS, ",") .. "}"
+			local PARTS = { }
+			for _, key in ipairs(object_keys) do
+			   local encoded_val = encode_value(self, TT[key],       parents, etc, indent)
+			   local encoded_key = encode_value(self, tostring(key), parents, etc, indent)
+			   table.insert(PARTS, string.format("%s:%s", encoded_key, encoded_val))
+			end
+			result_value = "{" .. table.concat(PARTS, ",") .. "}"
 
-         end
-      else
-         --
-         -- An empty array/object... we'll treat it as an array, though it should really be an option
-         --
-         result_value = "[]"
-      end
+		 end
+	  else
+		 --
+		 -- An empty array/object... we'll treat it as an array, though it should really be an option
+		 --
+		 result_value = "[]"
+	  end
 
-      parents[T] = false
-      return result_value
+	  parents[T] = false
+	  return result_value
    end
 end
 
 
 function OBJDEF:encode(value, etc)
    if type(self) ~= 'table' or self.__index ~= OBJDEF then
-      OBJDEF:onEncodeError("JSON:encode must be called in method format", etc)
+	  OBJDEF:onEncodeError("JSON:encode must be called in method format", etc)
    end
    return encode_value(self, value, {}, etc, nil)
 end
 
 function OBJDEF:encode_pretty(value, etc)
    if type(self) ~= 'table' or self.__index ~= OBJDEF then
-      OBJDEF:onEncodeError("JSON:encode_pretty must be called in method format", etc)
+	  OBJDEF:onEncodeError("JSON:encode_pretty must be called in method format", etc)
    end
    return encode_value(self, value, {}, etc, "")
 end
@@ -574,13 +576,15 @@ function OBJDEF:new(args)
    local new = { }
 
    if args then
-      for key, val in pairs(args) do
-         new[key] = val
-      end
+	  for key, val in pairs(args) do
+		 new[key] = val
+	  end
    end
 
    return setmetatable(new, OBJDEF)
 end
-local json = OBJDEF:new()
+function loadJson()
+	return OBJDEF:new()
+end
 
 return Parser
